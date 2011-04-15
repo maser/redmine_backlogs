bc7d2fed79cfe02b95a3c2ec0a016b3c67d44b07
Martin Luder <maser@winterfell.ch>
2011-04-15 15:21:52 +0200

    improved console output in migration 018_fix_positions_again, too

--- db/migrate/018_fix_story_positions_again.rb
+++ db/migrate/018_fix_story_positions_again.rb
@@ -1,7 +1,10 @@
 class FixStoryPositionsAgain < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
+      c = Story.count(:conditions => "parent_id is NULL")
+      puts "setting position for #{c} issues"
       Story.find(:all, :conditions => "parent_id is NULL", :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
+        puts "#{i} issues…" if i % 100 == 0 && i != 0
         s.position=i+1
         s.save!
       end
