284e12e560d3800754a63632d0c3ed9e03f31738
Martin Luder <maser@winterfell.ch>
2011-04-15 14:54:04 +0200

    better console output in migration 014_fix_positions

--- db/migrate/014_fix_positions.rb
+++ db/migrate/014_fix_positions.rb
@@ -11,14 +11,18 @@ class FixPositions < ActiveRecord::Migration
       puts "* due date is set, but start date is not"
       puts "* start date is later than due date"
       puts "* updated-date is before created-date"
-      raise "Please fix the issues with the follwing IDs and retry the migration"
+      puts "Please fix the issues with the following IDs and retry the migration"
       errors.each {|id|
         puts "- #{id}"
       }
+      raise "Please fix the issues with the printed IDs and retry the migration"
     end
 
     ActiveRecord::Base.transaction do
+      c = Story.count(:conditions => "parent_id is NULL")
+      puts "setting position for #{c} issues"
       Story.find(:all, :conditions => "parent_id is NULL", :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
+        puts "#{i} issues…" if i % 100 == 0 && i != 0
         s.position=i+1
         s.save!
       end
